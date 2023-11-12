d9a02de84dfdcb9a7a019f8842d69150882e96bd
Miguel-Nicolás Palau Lessa <miguelnicolas.palau@students.salle.url.edu>
2023-11-12 01:49:42 +0100

    add Initial, Join, Register views

--- src/views/Initial.vue
+++ src/views/Initial.vue
@@ -1,37 +1,73 @@
 <template>
     <main>
-
+        <h1>Battle Arena</h1>
+        <button>Join</button>
+        <button class="register">Register</button>
     </main>
 </template>
   
 <style scoped>
 
-#product-list {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    gap: 1.5rem;
-    align-items: baseline;
-    flex-wrap: wrap;
-}
-
 h1 {
-    display: block;
+    display: inline-block;
     text-align: center;
+    margin-top: 200px;
+    font-size: 100px;
 }
 
-.product {
-    width: 150px;
+button {
+    width:170px;
+    height:70px;
+    margin-top:25px;
+    margin: 1;
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #FAFBFC;
+    border: 1px solid rgba(27, 31, 35, 0.15);
+    border-radius: 40px;
+    box-shadow: rgba(27, 31, 35, 0.04) 0 1px 0, rgba(255, 255, 255, 0.25) 0 1px 0 inset;
+    box-sizing: border-box;
+    color: #24292E;
+    cursor: pointer;
+    display: inline-block;
+    padding: 6px 16px;
+    transition: background-color 0.2s cubic-bezier(0.3, 0, 0.5, 1);
+    touch-action: manipulation;
+    vertical-align: middle;
+    white-space: nowrap;
+    font-size: 17px;
 }
 
-.product img {
-    max-width: 150px;
+button:hover {
+    background-color: #F3F4F6;
+    transition-duration: 0.1s;
 }
 
-@media (min-width: 801px) {
-    h1 {
-        text-align: start;
-    }
+button:active {
+    background-color: #b8b8b8;
+    box-shadow: rgba(225, 228, 232, 0.2) 0 1px 0 inset;
+    transition: none 0s;
+}
+
+button:focus {
+    outline: 1px transparent;
+}
+
+.register {
+    background-color: rgb(117, 138, 255);
+    color: #ffffff;
+}
+
+.register:hover {
+    background-color: rgb(82, 108, 255);
+}
+
+.register:active {
+    background-color: rgb(22, 57, 255);
+    box-shadow: rgba(225, 228, 232, 0.2) 0 1px 0 inset;
+    transition: none 0s;
 }
 
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,65 @@
 <template>
     <main>
-
+        <h1>Player Registration</h1>
+        <form onsubmit="return validateForm()">
+            <label for="user">Username</label><br>
+            <input type="text" id="user" name="user"><br><br>
+            <label for="email">Email</label><br>
+            <input type="text" id="email" name="email"><br><br>
+            <label for="pwd">Password</label><br>
+            <input type="text" id="pwd" name="pwd"><br><br>
+            <label for="pwd-confirm">Password Confirmation</label><br>
+            <input type="text" id="pwd-confirm" name="pwd-confirm"><br><br>
+            <button type="submit">Register</button>
+        </form>
     </main>
 </template>
+  
+<style scoped>
+
+h1 {
+    display: inline-block;
+    text-align: center;
+    margin-top: 100px;
+    font-size: 70px;
+}
+
+form {
+    text-align: center;
+}
+
+button {
+    height:35px;
+    width:150px;
+    margin-top:15px;
+    position: relative;
+    background-color: rgb(117, 138, 255);
+    border: 1px solid rgba(27, 31, 35, 0.15);
+    border-radius: 40px;
+    box-shadow: rgba(27, 31, 35, 0.04) 0 1px 0, rgba(255, 255, 255, 0.25) 0 1px 0 inset;
+    box-sizing: border-box;
+    color: #ffffff;
+    cursor: pointer;
+    display: inline-block;
+    padding: 6px 16px;
+    touch-action: manipulation;
+    white-space: nowrap;
+    font-size: 15px;
+}
+
+button:hover {
+    background-color: rgb(82, 108, 255);
+    transition-duration: 0.1s;
+}
+
+button:active {
+    background-color: rgb(22, 57, 255);
+    box-shadow: rgba(225, 228, 232, 0.2) 0 1px 0 inset;
+    transition: none 0s;
+}
+
+button:focus {
+    outline: 1px transparent;
+}
+
+</style>
