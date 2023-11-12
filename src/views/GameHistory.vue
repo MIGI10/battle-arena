70fd321737d3454899823baece20ea772dcb7c82
adrianjorge.sanchez <adrianjorge.sanchez@students.salle.url.edu>
2023-11-12 18:54:26 +0100

    settings dropdown implemented in all views

--- src/views/GameHistory.vue
+++ src/views/GameHistory.vue
@@ -3,7 +3,14 @@
     <div class="player-game-history">
         <div class="top-bar">      
             <button class="back-btn"><img class="icon" src="./../assets/back.png"></button>
-        <button class="setting-button"><img class="icon" src="./../assets/settings.png"></button>
+            <div class="settings-container">
+                <button @click="toggleDropdownSett" class="setting-button"><img class="icon" src="./../assets/settings.png"></button>
+                <div v-if="showDropdownSett" class="dropdown-optionsSett">
+                    <div v-for="option in stateOptionsSett" :key="option" @click="selectStateSett(option)">
+                                {{ option }}
+                    </div>
+                </div>
+            </div>
         </div>
         <h1 class="title">Player {{ playerName }}'s Game History</h1>
         <div class="percentage-bar">
@@ -22,7 +29,17 @@ export default {
         return {
             playerName: "John",
             percentage: 60,
+            showDropdownSett: false,
+            stateOptionsSett: ["Log out", "Delete Account"]
         };
+    },
+    methods: {
+        toggleDropdownSett() {
+            this.showDropdownSett = !this.showDropdownSett;
+        },
+        selectStateSett(option) {
+            this.showDropdownSett = false;
+        },
     },
 };
 </script>
@@ -30,6 +47,41 @@ export default {
 <style scoped>
 .player-game-history {
     text-align: center;
+}
+
+.settings-container {
+  position: relative;
+  display: inline-block;
+}
+
+.state-dropdownSett {
+    border: 1px solid #ccc;
+    cursor: pointer;
+    margin: 0 auto;
+}
+
+.dropdown-optionsSett {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    border: 1px solid #ccc;
+    padding: 5px;
+    margin: 0 auto;
+}
+
+.dropdown-optionsSett div {
+    cursor: pointer;
+}
+
+.dropdown-optionsSett div:hover {
+    background-color: rgb(82, 108, 255);
+    transition-duration: 0.1s;
+}
+
+.dropdown-optionsSett div:active {
+    background-color: rgb(22, 57, 255);
+    box-shadow: rgba(225, 228, 232, 0.2) 0 1px 0 inset;
+    transition: none 0s;
 }
 
 .title {
--- src/views/Games.vue
+++ src/views/Games.vue
@@ -3,7 +3,14 @@
         <div class="game-finder">
         <div class="top-bar">      
             <button class="back-btn"><img class="icon" src="./../assets/back.png"></button>
-            <button class="setting-button"><img class="icon" src="./../assets/settings.png"></button>
+            <div class="settings-container">
+                <button @click="toggleDropdownSett" class="setting-button"><img class="icon" src="./../assets/settings.png"></button>
+                <div v-if="showDropdownSett" class="dropdown-optionsSett">
+                    <div v-for="option in stateOptionsSett" :key="option" @click="selectStateSett(option)">
+                                {{ option }}
+                    </div>
+                </div>
+            </div>
         </div>
         <h1 class="title">Game Finder</h1>
         
@@ -42,28 +49,71 @@
 
 <script>
 export default {
-  data() {
-    return {
-      selectedState: "Select State",
-      showDropdown: false,
-      stateOptions: ["Available", "Finished", "All"],
-    };
-  },
-  methods: {
-    toggleDropdown() {
-      this.showDropdown = !this.showDropdown;
+    data() {
+        return {
+            selectedState: "Select State",
+            showDropdown: false,
+            stateOptions: ["Available", "Finished", "All"],
+            showDropdownSett: false,
+            stateOptionsSett: ["Log out", "Delete Account"]
+        };
     },
-    selectState(option) {
-      this.selectedState = option;
-      this.showDropdown = true;
+    methods: {
+        toggleDropdown() {
+            this.showDropdown = !this.showDropdown;
+        },
+        selectState(option) {
+            this.selectedState = option;
+            this.showDropdown = true;
+        },
+        toggleDropdownSett() {
+            this.showDropdownSett = !this.showDropdownSett;
+        },
+        selectStateSett(option) {
+            this.showDropdownSett = false;
+        },
     },
-  },
 };
 </script>
 
 <style scoped>
 .game-finder {
     text-align: center;
+}
+
+.settings-container {
+  position: relative;
+  display: inline-block;
+}
+
+.state-dropdownSett {
+    border: 1px solid #ccc;
+    cursor: pointer;
+    margin: 0 auto;
+}
+
+.dropdown-optionsSett {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    border: 1px solid #ccc;
+    padding: 5px;
+    margin: 0 auto;
+}
+
+.dropdown-optionsSett div {
+    cursor: pointer;
+}
+
+.dropdown-optionsSett div:hover {
+    background-color: rgb(82, 108, 255);
+    transition-duration: 0.1s;
+}
+
+.dropdown-optionsSett div:active {
+    background-color: rgb(22, 57, 255);
+    box-shadow: rgba(225, 228, 232, 0.2) 0 1px 0 inset;
+    transition: none 0s;
 }
 
 .title {
